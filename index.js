5414ff6b72b305f7d1fbc954ec5e35f537022936
Anik Mosfeq <mosfeqanik01@gmail.com>
2022-07-12 18:15:06 +0600

    class 1 practice added

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 alert('hello world');
 var a= 15.344;
 console.log(a);
+
+var a = 10;
+console.log(a);
+
+var a = [1,2,3,"habib","100",15.02,[4,"faisal"]];
+console.log(a);
+
+var c =10;
+console.log(c);
+
+var b = "habib";
+console.log(b);
+
+var d=[1,2,"a"];
+console.log(d);
+
+var e =d;
+console.log(e);
+
+function add(a,b){
+    console.log(a+b);
+}
+add(3,5);
+
+for(var i=1;i<=10;i++){
+    console.log(i);
+}
+
+var k =-12.45;
+if(k<0){
+    console.log(k +" is negative" );
+}else{
+    console.log(k + " is positive");
+}
+var isclicked= true;
+if(isclicked){
+    console.log("On");
+}else{
+    console.log("off");
+}
